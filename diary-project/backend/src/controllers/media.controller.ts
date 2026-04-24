e3defaa84bbc7fd504404c3ed94abd4d8d788d3e
Nereag443 <nerea12172005@gmail.com>
2026-04-24 10:33:56 +0200

    añadido controladores de medios

---
+++ diary-project/backend/src/controllers/media.controller.ts
@@ -0,0 +1,37 @@
+import * as mediaService from '../services/media.service';
+import { Request, Response, NextFunction } from 'express';
+
+export const getMedia = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const media = await mediaService.getAllMedia();
+        res.json(media);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const  createMedia = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { title, type, rating, review, user_id} = (req.body);
+        if (!title || title.trim() === '') {
+            return res.status(400).json({ error: 'Title is required' });
+        }
+        if (!type || type.trim() === '') {
+            return res.status(400).json({ error: 'Type is required' });
+        }
+        const media = await mediaService.createMedia({ title, type, rating, review, user_id });
+        res.status(201).json(media);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteMedia = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = Number(req.params.id);
+        await mediaService.deleteMedia(id);
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+};
