c0af4efc36e5f3f2c89e352e7a3c08676206e539
Nereag443 <nerea12172005@gmail.com>
2026-04-24 10:33:21 +0200

    añadido media services

---
+++ diary-project/backend/src/services/media.service.ts
@@ -0,0 +1,56 @@
+import { db } from '../config/db';
+
+export interface Media {
+    id: number;
+    title: string;
+    type: string;
+    rating: number;
+    review: string | null;
+    date_added: string;
+    user_id: number;
+}
+
+export const getAllMedia = (): Promise<Media[]> => {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM media', [], (err, rows: Media[]) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
+
+export const createMedia = (media: Omit<Media, 'id' | 'date_added'>): Promise<Media> => {
+    return new Promise((resolve, reject) => {
+        const date = new Date().toISOString();
+        db.run(
+            `INSERT INTO media (title, type, rating, review, user_id, date_added) VALUES (?, ? ,?, ?, ?, ?)`,
+        [media.title, media.type, media.rating, media.review, media.user_id, date],
+        function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({
+                    id: this.lastID,
+                    ...media, date_added: date
+                });
+            }
+        });
+    });
+};
+
+export const deleteMedia = (id: number): Promise<void> => {
+    return new Promise((resolve, reject ) => {
+        db.run('DELETE FROM media WHERE id = ?', [id], function (err) {
+            if (err) {
+                reject (err);
+            }else if (this.changes === 0) {
+                reject(new Error('Media not found'));
+            }else {
+                resolve();
+            }
+        });
+    });
+};
