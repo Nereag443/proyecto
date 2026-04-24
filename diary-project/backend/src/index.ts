366cd9f7a3f8a49a060f2ee7aa569154d319a03d
Nereag443 <nerea12172005@gmail.com>
2026-04-24 10:37:06 +0200

    añadido server Express

---
+++ diary-project/backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import mediaRoutes from './routes/media.routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1/media', mediaRoutes);
+
+app.use ((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err);
+    if (err.message === 'NOT_FOUND') {
+        return res.status(404).json({ error: 'Not Found' });
+    }
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+app.listen(3001, () => {
+    console.log('Server is running on http://localhost:3001');
+});
+
+export default app;
