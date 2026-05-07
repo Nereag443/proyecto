f7271cc76fd7200c21a30ebb6ea535795198f6bf
Nereag443 <nerea12172005@gmail.com>
2026-05-07 11:10:41 +0200

    Añadido mensaje de carga del servidor

--- diary-project/frontend/src/components/spinner.tsx
+++ diary-project/frontend/src/components/spinner.tsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+
+
 export function Spinner() {
+    const [showWakeUpMessage, setShowWakeUpMessage] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowWakeUpMessage(true);
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className="flex items-center justify-center h-full w-full">
+        <div className="flex flex-col gap-4 items-center justify-center h-full w-full">
             <i className="fa-solid fa-spinner fa-spin text-4xl text-gray-500"></i>
+            {showWakeUpMessage && (
+                <p className="text-gray-500">Iniciando el servidor…</p>
+            )}
         </div>
     );
 }
